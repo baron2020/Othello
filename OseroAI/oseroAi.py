53374734d11ef778876ec9e457a1f124882ef991
k.yuka <61102173+baron2020@users.noreply.github.com>
2020-04-18 21:07:38 +0900

    Add files via upload

---
+++ OseroAI/oseroAi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 14:17:54 2020
+
+@author: barosan
+"""
+import tkinter as tk
+window=tk.Tk()
+window.title('オセロ')
+#window.gemetry('360*360')
+window.mainloop()
