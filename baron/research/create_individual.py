73aa15b4818f84f94659a381042ee0f3b57f540e
k.yuka <61102173+baron2020@users.noreply.github.com>
2020-06-11 22:17:30 +0900

    Add files via upload

--- baron/research/create_individual.py
+++ baron/research/create_individual.py
@@ -6,49 +6,60 @@ Created on Wed Jun  3 21:30:11 2020
 """
 
 import numpy as np
+import os
 import csv
 import datetime
 import random
 
-dt_now=datetime.datetime.now()
-# print(dt_now)#.strftime('%Y年%m月%d日 %H:%M:%S')
-hyouka_array=[[0]*8 for i in range(8)]#全体の評価関数
-temp_array=[]#1個の評価関数
-kansu_index=[[1,2,3,4,4,3,2,1],
-             [2,5,6,7,7,6,5,2],
-             [3,6,8,9,9,8,6,3],
-             [4,7,9,10,10,9,7,4],
-             [4,7,9,10,10,9,7,4],
-             [3,6,8,9,9,8,6,3],
-             [2,5,6,7,7,6,5,2],
-             [1,2,3,4,4,3,2,1]
-             ]
+class CreateIndividual():
+    def __init__(self):
+        self.dt_now=datetime.datetime.now()#現在の時刻
+		# print(dt_now)#.strftime('%Y年%m月%d日 %H:%M:%S')
+        self.individual=[[0]*8 for i in range(8)]#個体=評価関数
+        self.all_gene=[]#1個体の全ての遺伝子
+        self.gene_index=[[1,2,3,4,4,3,2,1],
+                         [2,5,6,7,7,6,5,2],
+                         [3,6,8,9,9,8,6,3],
+                         [4,7,9,10,10,9,7,4],
+                         [4,7,9,10,10,9,7,4],
+                         [3,6,8,9,9,8,6,3],
+                         [2,5,6,7,7,6,5,2],
+                         [1,2,3,4,4,3,2,1]
+                         ]
+        
+        for i in range(10):
+            random_gene=random.randint(-150,150)
+            self.all_gene.append(random_gene)
+        for y in range(8):
+            for x in range(8):    
+                self.individual[y][x]=self.all_gene[self.gene_index[y][x]-1]
 
-for i in range(10):
-    temp_hyouka=random.randint(-150,150)
-    temp_array.append(temp_hyouka)
-# for i in temp_array:
-#     print(i)
-for y in range(8):
-    for x in range(8):
-       hyouka_array[y][x]=temp_array[kansu_index[y][x]-1]
+        self.individual_np_array=np.array(self.individual)
+        print(self.individual_np_array)#評価関数
+        
+    def writing_csv(self):
+        with open('research/generation1/test0610.csv','w',encoding='utf-8') as file:
+            writer=csv.writer(file,lineterminator='\n')#改行コードの指定
+            writer.writerow([self.dt_now])#ヘッダー
+            writer.writerows(self.individual)#複数行の書き込み
 
-hyouka_np_array=np.array(hyouka_array)
-print(hyouka_np_array)
-# array_sum=np.sum(hyouka_np_array)
-# print(array_sum)
+    def reading_csv(self):
+        with open('research/generation1/test0610.csv','r',encoding='utf-8') as fp:
+            csv_list=list(csv.reader(fp))
+        print(csv_list)
+        print(csv_list[3][3])
 
-
-with open('評価関数/テスト/test0609.csv','w',encoding='utf-8') as file:
-    writer=csv.writer(file,lineterminator='\n')#改行コードの指定
-    writer.writerow([dt_now])#ヘッダー
-    writer.writerows(hyouka_array)#複数行の書き込み
-
-with open('評価関数/テスト/test0609.csv','r',encoding='utf-8') as fp:
-    lst=list(csv.reader(fp))
-    
-print(lst)
-print(lst[0][0])
-print(type(lst[0]))
-
-
+    def all_create_generation(self):
+        print("aaa")
+        with open('research/record.csv','r',encoding='utf-8') as f:
+            generation_name=f.read()
+        print(generation_name)
+        target_dir=os.path.join("C:/Users/barosan/Desktop/BaronOsero/research/",generation_name)
+        print(target_dir)
+        print(os.path.isdir(target_dir))
+        
+if __name__=="__main__":
+    ci=CreateIndividual()
+    # ci.writing_csv()
+    # ci.reading_csv()
+    ci.all_create_generation()
