ec13170129a8ade050c907647c1b41525836f12b
k.yuka <61102173+baron2020@users.noreply.github.com>
2020-06-13 22:58:57 +0900

    Add files via upload

--- baron/research/create_individual.py
+++ baron/research/create_individual.py
@@ -15,6 +15,8 @@ class CreateIndividual():
     def __init__(self):
         self.dt_now=datetime.datetime.now()#現在の時刻
 		# print(dt_now)#.strftime('%Y年%m月%d日 %H:%M:%S')
+
+    def create_evalution_function(self):
         self.individual=[[0]*8 for i in range(8)]#個体=評価関数
         self.all_gene=[]#1個体の全ての遺伝子
         self.gene_index=[[1,2,3,4,4,3,2,1],
@@ -26,7 +28,6 @@ class CreateIndividual():
                          [2,5,6,7,7,6,5,2],
                          [1,2,3,4,4,3,2,1]
                          ]
-        
         for i in range(10):
             random_gene=random.randint(-150,150)
             self.all_gene.append(random_gene)
@@ -37,8 +38,9 @@ class CreateIndividual():
         self.individual_np_array=np.array(self.individual)
         print(self.individual_np_array)#評価関数
         
-    def writing_csv(self):
-        with open('research/generation1/test0610.csv','w',encoding='utf-8') as file:
+    def writing_csv(self,path):
+        temp_csv=path+'.csv'
+        with open('research/'+temp_csv,'w',encoding='utf-8') as file:
             writer=csv.writer(file,lineterminator='\n')#改行コードの指定
             writer.writerow([self.dt_now])#ヘッダー
             writer.writerows(self.individual)#複数行の書き込み
@@ -49,29 +51,33 @@ class CreateIndividual():
         print(csv_list)
         print(csv_list[3][3])
 
-    def all_create_generation(self):
-        print("aaa")
-        with open('research/record.csv','r',encoding='utf-8') as file:
-            generation_name=file.read().strip()
-        print(generation_name)
-        
-        target_dir="C:/Users/barosan/Desktop/BaronOsero/research/"+generation_name
-
-        print(target_dir)
-        if os.path.isdir(target_dir)==False:
-            os.mkdir(target_dir)
-        else:
-            print("何もしない")
-            
-    def writing_record_csv(self,temp):
+    def update_record_csv(self,temp):   
         with open('research/record.csv','w',encoding='utf-8') as file:
             writer=csv.writer(file)
             writer.writerow([temp])
-        
-if __name__=="__main__":
-    ci=CreateIndividual()
-    # ci.writing_csv()
-    # ci.reading_csv()
-    ci.all_create_generation()
-    ci.writing_record_csv("generation3") 
+
+    def all_create_generation(self):
+        with open('research/record.csv','r',encoding='utf-8') as file:
+            generation_name=file.read().strip()
+        # print(generation_name)
+        target_dir="C:/Users/barosan/Desktop/BaronOsero/research/"+generation_name
+        # print(target_dir)
+        temp_head=generation_name[0:-1]
+        temp_tail=generation_name[-1]
+
+        if os.path.isdir(target_dir)==False:
+            os.mkdir(target_dir)
+            if generation_name=="generation1":
+                print('初期世代')
+                for i in range(1,21):
+                    path=generation_name+'/individual'+str(i)
+                    # print(i)
+                    self.create_evalution_function()
+                    self.writing_csv(path)
+            elif generation_name!="generation1":
+                print('2世代以降の処理')
+            self.update_record_csv(temp_head+str(int(temp_tail)+1))
+        else:
+            print("存在するので何もしない")
+
     
---
+++ baron/research/research_motion.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 22:02:46 2020
+
+@author: barosan
+"""
+from research import create_individual
+
+if __name__=="__main__":
+    ci=create_individual.CreateIndividual()
+    ci.all_create_generation()
