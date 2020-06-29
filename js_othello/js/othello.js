11755487230c4e72127e3f3aee194e83b0d9ba04
k.yuka <61102173+baron2020@users.noreply.github.com>
2020-06-29 23:29:17 +0900

    Add files via upload

---
+++ js_othello/js/othello.js
@@ -0,0 +1,95 @@
+﻿
+//スタート
+function start(){
+	userCheck();
+	mainAria();
+
+
+	}
+
+//ユーザーチェック
+function userCheck(){
+	let userB=window.navigator.appName;//ユーザーブラウザ
+	let wnu=window.navigator.userAgent;//ユーザーエージェント
+	let userOs;//ユーザーos
+	let userW=document.documentElement.clientWidth;//window.innerWidth;//ウィンドウの横幅
+	let userH=document.documentElement.clientHeight;//window.innerHeight;//ウィンドウの高さ
+	if(wnu.indexOf('iPhone')!=-1){
+		userOs="iPhone";
+	}else if(wnu.indexOf('iPod')!=-1){
+		userOs="iPod";
+	}else if(wnu.indexOf('Android')!=-1){
+		userOs="Android";
+	}else if(wnu.indexOf('Windows')!=-1){
+		userOs="Windows";
+	}else{
+		userOs="わかりません";
+	}
+	document.getElementById("useros").innerHTML="OS："+userOs;//userのosを表示
+	document.getElementById("userw").innerHTML="横幅："+userW;//userの横幅を表示
+	document.getElementById("userh").innerHTML="高さ："+userH;//userの高さを表示
+}
+
+//中央メイン盤の作成
+function mainAria(){
+	let mainDisplay="<table id='board' border='0'align='center'>";//メイン表示用
+	let mainDisplayTop="<table border='0'align='center'>";//上用
+	let mainDisplayBottom="<table border='0'align='center'>";//下用
+	//main
+	for(let y=1;y<9;y++){
+		for(let x=1;x<9;x++){
+			if(x==1){
+				mainDisplay+="<tr>";
+			}
+			mainDisplay+="<td class='ban'id='d"+y+"s"+x+"'></td>";
+			if(x==8){
+				mainDisplay+="</tr>";
+			}
+			if((y==8)&&(x==8)){
+				mainDisplay+="</table>";
+				break;
+			}
+		}
+	}
+	//top
+	for(let i=1;i<9;i++){
+		if(i==1){
+			mainDisplayTop+="<tr>";
+		}
+		mainDisplayTop+="<td class='topEdge'id='d0s"+i+"'></td>";
+		if(i==8){
+			mainDisplayTop+="</tr></table>";
+			break;
+		}
+	}
+	//bottom
+	for(let i=1;i<9;i++){
+		if(i==1){
+			mainDisplayBottom+="<tr>";
+		}
+		mainDisplayBottom+="<td class='bottomEdge'id='d10s"+i+"'></td>";
+		if(i==8){
+			mainDisplayBottom+="</tr></table>";
+			break;
+		}
+	}
+	document.getElementById("mainDispTop").innerHTML=mainDisplayTop;
+	document.getElementById("mainDisp").innerHTML=mainDisplay;
+	document.getElementById("mainDispBottom").innerHTML=mainDisplayBottom;
+}
+
+//パソコン用マウスダウン
+function mousedown(e){
+	touchScreen(e.clientX,e.clientY);
+}
+//スマホ用タッチスタート
+function touchstart(e){
+	//もしタッチされたのが一箇所であるなら
+	if(e.targetTouches.length==1){
+		touch=e.targetTouches[0];
+		touchScreen(touch.clientX,touch.clientY);
+	}
+}
+//start()系終了---------------------------------------------------------------------------------------
+
+
