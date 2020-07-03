bed8cdc2f2a12bbbb55675a5353d994dbe8d6e67
k.yuka <61102173+baron2020@users.noreply.github.com>
2020-07-04 00:34:48 +0900

    Add files via upload

--- js_othello/js/othello.js
+++ js_othello/js/othello.js
@@ -1,4 +1,38 @@
-﻿
+﻿//グローバル関数
+//座標関連
+var Page ={ cy:0,//現在のy
+			cx:0,//現在のx
+			cys:0,//現在の整数y
+			cxs:0,//現在の整数x
+			
+			};
+//ゲームの情報
+var Game ={ teban:"先手",
+			rivalTeban:"後手",//逆の手番
+			count:1,//何手目か？
+			currentPieceId:"",//現在の駒のId
+			currentPieceName:"",//現在の駒(二文字)
+			currentPieceClass:"",//現在の駒クラス
+			currentMasu:"",//現在のマスのid
+			firstTouchPieceId:"",//一度目にタッチした駒のid
+			firstTouchPieceName:"",//一度目にタッチした駒(二文字)
+			firstTouchMasu:""//一度目にタッチしたマスのid
+			};
+//フラグ
+var Flg = { //強制終了フラグ
+			tumi:false,//詰んでいる？
+			endMode:false,//false:対局モード中
+			//選択後
+			firstTouchMasuInOut:false,//最初にタッチしたマスは盤内か？
+			currentMasuInout:false,//現在のマスは盤内か？
+			firstChoice:true,//最初に駒を選択できる状態:true
+			firstPromotion:false,//最初に選択した駒は成駒か？
+			get:false,//駒をとっていない
+			
+			//棋譜用
+			kihuFirstTouch:true//棋譜用に使うフラグ、最初にタッチできる時:true
+			};
+
 //スタート
 function start(){
 	let supportTouch='ontouchend'in document;//タッチイベントがサポートされているか
@@ -137,6 +171,33 @@ function touchScreen(tx,ty){
 //座標取得
 function getCoordinate(tx,ty){
 	console.log("テスト")
+	let d1s1Element=document.getElementById("d1s1");
+	let d1s1rect=d1s1Element.getBoundingClientRect();
+	
+	console.log("d1s1までの横"+Math.round(d1s1rect.left));//四捨五入
+	console.log("d1s1までの高さ"+Math.round(d1s1rect.top));
+	console.log("d1s1の幅"+d1s1rect.width);
+	console.log("d1s1の高さ"+d1s1rect.height);
+
+	let banX=Math.round(d1s1rect.left);//将棋盤(d1s1)までの横幅(距離)
+	let banY=Math.round(d1s1rect.top);//将棋盤(d1s1)までの高さ(距離)
+	console.log(banX);
+	console.log(banY);
+
+	Page.cx=Math.floor(tx);
+	Page.cy=Math.floor(ty);
+	Page.cxs=Math.floor(((tx-banX)/d1s1rect.width)+1);
+	Page.cys=Math.floor(((ty-banY)/d1s1rect.height)+1);
+
+	Game.currentMasu="d"+Page.cys+"s"+Page.cxs;//カレントのタッチマス
+	//Flg.currentMasuInout=InOut(Page.cys,Page.cxs);//カレントのマスは盤内？盤外？
+	//y,x座標の表示
+	document.getElementById("ky").innerHTML=Page.cy;//y座標
+	document.getElementById("kx").innerHTML=Page.cx;//x座標
+	
+	document.getElementById("cm").innerHTML=Game.currentMasu;//カレントのタッチマス
+	//document.getElementById("bnai").innerHTML=Flg.currentMasuInout;//カレントのマスは盤内？盤外？
+
 }
 
 
