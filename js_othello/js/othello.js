4637d29415f669605c87194933122095343e9d3b
k.yuka <61102173+baron2020@users.noreply.github.com>
2020-07-08 00:09:56 +0900

    Add files via upload

--- js_othello/js/othello.js
+++ js_othello/js/othello.js
@@ -1,8 +1,26 @@
 ﻿//グローバル関数
-
+//盤面情報
+var gameRecode={'d1s1':'None','d1s2':'None','d1s3':'None','d1s4':'None','d1s5':'None','d1s6':'None','d1s7':'None','d1s8':'None',
+				'd2s1':'None','d2s2':'None','d2s3':'None','d2s4':'None','d2s5':'None','d2s6':'None','d2s7':'None','d2s8':'None',
+				'd3s1':'None','d3s2':'None','d3s3':'None','d3s4':'None','d3s5':'None','d3s6':'None','d3s7':'None','d3s8':'None',
+				'd4s1':'None','d4s2':'None','d4s3':'None','d4s4':'white','d4s5':'black','d4s6':'None','d4s7':'None','d4s8':'None',
+				'd5s1':'None','d5s2':'None','d5s3':'None','d5s4':'black','d5s5':'white','d5s6':'None','d5s7':'None','d5s8':'None',
+				'd6s1':'None','d6s2':'None','d6s3':'None','d6s4':'None','d6s5':'None','d6s6':'None','d6s7':'None','d6s8':'None',
+				'd7s1':'None','d7s2':'None','d7s3':'None','d7s4':'None','d7s5':'None','d7s6':'None','d7s7':'None','d7s8':'None',
+				'd8s1':'None','d8s2':'None','d8s3':'None','d8s4':'None','d8s5':'None','d8s6':'None','d8s7':'None','d8s8':'None'
+				}
+//盤面情報のキー
+var gameRecodeKeys=['d1s1','d1s2','d1s3','d1s4','d1s5','d1s6','d1s7','d1s8',
+					'd2s1','d2s2','d2s3','d2s4','d2s5','d2s6','d2s7','d2s8',
+					'd3s1','d3s2','d3s3','d3s4','d3s5','d3s6','d3s7','d3s8',
+					'd4s1','d4s2','d4s3','d4s4','d4s5','d4s6','d4s7','d4s8',
+					'd5s1','d5s2','d5s3','d5s4','d5s5','d5s6','d5s7','d5s8',
+					'd6s1','d6s2','d6s3','d6s4','d6s5','d6s6','d6s7','d6s8',
+					'd7s1','d7s2','d7s3','d7s4','d7s5','d7s6','d7s7','d7s8',
+					'd8s1','d8s2','d8s3','d8s4','d8s5','d8s6','d8s7','d8s8'
+					]
 //石の配列(黒,白)
 var stone=["<p class='stone' id='black'></p>","<p class='stone' id='white'></p>"]
-
 //座標関連
 var Page ={ cy:0,//現在のy
 			cx:0,//現在のx
@@ -14,6 +32,8 @@ var Page ={ cy:0,//現在のy
 var Game ={ teban:"黒",
 			rivalTeban:"白",//逆の手番
 			count:1,//何手目か？
+			blackNum:2,//黒石の数
+			whiteNum:2,//白石の数
 			currentPieceId:"",//現在の駒のId
 			currentPieceName:"",//現在の駒(二文字)
 			currentPieceClass:"",//現在の駒クラス
@@ -55,8 +75,6 @@ function start(){
 	}
 }
 
-
-
 //ユーザーチェック
 function userCheck(){
 	let userB=window.navigator.appName;//ユーザーブラウザ
@@ -82,8 +100,14 @@ function userCheck(){
 
 //開始時の表示
 function startDisplay(){
-	document.getElementById("teban").innerHTML=Game.teban;//手番の表示
+	document.getElementById("teban").innerHTML=Game.teban+"の手番です";//手番の表示
 	document.getElementById("gamecount").innerHTML=Game.count+"手目";//何手目の表示
+	document.getElementById("blackNum").innerHTML="黒石："+Game.blackNum;//黒石の数
+	document.getElementById("whiteNum").innerHTML="白石："+Game.whiteNum;//白石の数
+	
+	tempInp1="<input class='con'id='re1' type='button' value='ページのリロード'onClick='inputContinue()'style='width:37%'>";
+	tempInp2="<input class='con'id=='de1' type='button' value='ログ削除'onClick='inputLogDelete()'style='width:23%'>";
+	document.getElementById("consider").innerHTML=tempInp1+tempInp2;
 }
 
 //中央メイン盤の作成
@@ -159,7 +183,6 @@ function setUp(){
 	document.getElementById("d4s5").insertAdjacentHTML('afterbegin',stone[0]);
 	document.getElementById("d5s4").insertAdjacentHTML('afterbegin',stone[0]);
 	document.getElementById("d5s5").insertAdjacentHTML('afterbegin',stone[1]);
-
 }
 
 //パソコン用マウスダウン
@@ -179,23 +202,22 @@ function touchstart(e){
 //touchScreen()系開始----------------------------------------------------------------------------------
 //タッチされた時のイベントの処理
 function touchScreen(tx,ty){
-	getCoordinate(tx,ty);//座標、盤内外の取得
-	document.getElementById("test").innerHTML="タッチされました";
-	
-	if(Flg.currentMasuInout==true){
+	getCoordinate(tx,ty);//座標、盤内外の取得	
+	if((Flg.currentMasuInout==true)&&(gameRecode[Game.currentMasu]=='None')){
 		console.log('test:'+Flg.currentMasuInout);//盤内
 		console.log('現在のマス:'+Game.currentMasu);
+		console.log('js内の盤の石:'+gameRecode[Game.currentMasu]);//js内の石
 		tyakusyu(Game.currentMasu);
 	}else{
 		console.log('test:'+Flg.currentMasuInout);//盤外
+		console.log('js内の盤の石:'+gameRecode[Game.currentMasu]);//js内の石
 		return;
 	}
-	tyakusyuEnd();//手番&何手目の進行
+	changeTeban();//手番の切り替えに伴う処理
 }
 
 //座標取得
 function getCoordinate(tx,ty){
-	//console.log("テスト")
 	let d1s1Element=document.getElementById("d1s1");
 	let d1s1rect=d1s1Element.getBoundingClientRect();
 	//console.log("d1s1までの横"+Math.round(d1s1rect.left));//四捨五入
@@ -215,8 +237,6 @@ function getCoordinate(tx,ty){
 	Game.currentMasu="d"+Page.cys+"s"+Page.cxs;//カレントのタッチマス
 	//Flg.currentMasuInout=InOut(Page.cys,Page.cxs);//カレントのマスは盤内？盤外？
 	//y,x座標の表示
-	document.getElementById("cy").innerHTML=Page.cy;//y座標
-	document.getElementById("cx").innerHTML=Page.cx;//x座標
 	document.getElementById("cMasu").innerHTML=Game.currentMasu;//カレントのタッチマス
 	Flg.currentMasuInout=inOut(Page.cys,Page.cxs);
 	document.getElementById("inOut").innerHTML=Flg.currentMasuInout;//カレントのマスは盤内？盤外？
@@ -232,25 +252,63 @@ function inOut(targetY,targetX){
 
 //着手したマスに石を置く
 function tyakusyu(setMasu){
-	if(	Game.teban=='黒'){
+	if(Game.teban=='黒'){
 		targetStone=stone[0];
+		gameRecodeStone='black';
 	}else if(Game.teban=='白'){
 		targetStone=stone[1];
+		gameRecodeStone='white';
 	}
 	document.getElementById(setMasu).insertAdjacentHTML('afterbegin',targetStone);
+	gameRecode[Game.currentMasu]=gameRecodeStone;
+	return;
 }
 
 //着手終了の処理
-function tyakusyuEnd(){
+function changeTeban(){
 	if(	Game.teban=='黒'){
 		Game.teban='白';
 	}else if(Game.teban=='白'){
 		Game.teban='黒';
 	}
 	Game.count++;
-	document.getElementById("teban").innerHTML=Game.teban;//手番の表示
+	document.getElementById("teban").innerHTML=Game.teban+"の手番です";//手番の表示
 	document.getElementById("gamecount").innerHTML=Game.count+"手目";//何手目の表示
+	checkStoneNum();
 	return;
 }
 
+//着手終了時の石の数の確認＆表示
+function checkStoneNum(){
+	//着手完了後に、石の数の確認をし,石の数のテキストを更新する。
+	let tempBlackNum=0;
+	let tempWhiteNum=0;
+	for(let i=0;i<gameRecodeKeys.length;i++){
+		if(gameRecode[gameRecodeKeys[i]]=='black'){
+			tempBlackNum++;
+		}
+		if(gameRecode[gameRecodeKeys[i]]=='white'){
+			tempWhiteNum++;
+		}
+	}
+	//console.log("黒石の数"+tempBlackNum)
+	//console.log("白石の数"+tempWhiteNum)
+	Game.blackNum=tempBlackNum;
+	Game.whiteNum=tempWhiteNum;
+	document.getElementById("blackNum").innerHTML="黒石："+Game.blackNum;//黒石の数
+	document.getElementById("whiteNum").innerHTML="白石："+Game.whiteNum;//白石の数
+}
 
+function winLoseJudgment(){
+	//着手完了後に、勝敗が着いているか調べる。
+	//勝敗が着いている場合は、手番テキストを更新し、終了フラグを立てる。
+}
+
+//continue:リロード
+function inputContinue(){
+	window.location.reload();
+}
+//コンソール削除
+function inputLogDelete(){
+	console.clear();
+}
